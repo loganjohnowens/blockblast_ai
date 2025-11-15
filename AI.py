8a6710269b18d2d58140e365eac4cbce70f19916
Logan <185567349+loganjohnowens@users.noreply.github.com>
2025-11-15 10:09:44 -0700

    added_dictonary_of_shapes

--- AI.py
+++ AI.py
@@ -15,90 +15,88 @@ def get_pieces():
     global slot_1
     global slot_2
     global slot_3
-    peaces_1 = {
-        'no block in slot 1': False
+    peaces = {
+        'no block in slot 1': False,
+        'no block in slot 2': False,
+        'no block in slot 3': False,
         'square': [[
             1, 1,
-            1, 1 ], [,2 2]
-        ]
-        '3 by 2 horizontal': 1
-        '5 long line': 1
-        '4 tall line': 1
-        'big corner in the botom right': 1
-        'T': 1
+            1, 1], [2, 2]
+        ],
+        '3 by 2 horizontal':
+            [[1, 1, 1,
+              1, 1, 1], [3, 2]],
+        '5 long line':
+            [[1, 1, 1, 1, 1,], [5, 1]],
+        '4 tall line':
+            [[1,
+              1,
+              1,
+              1, ], [1, 4]],
+        'big corner in the botom right':
+            [[0, 0, 1,
+              0, 0, 1,
+              1, 1, 1], [3, 3]],
+        'T':
+            [[1, 1, 1,
+              0, 1, 0,
+             0, 1, 0], [3, 3]],
         '2 by 3 vertical': 1
-        '2 tall line': 1
-        'top left corner': 1
-        'big top right corner': 1
-        'J pointed right': 1
-        '9 by 9 square': 1
+            [[1, 1,
+              1, 1,
+              1, 1], [2, 3]],
+        '2 tall line':
+            [[1,
+              1], [1, 2]],
+        'top left corner':
+            [[1, 1,
+              1, 0], [2, 2]],
+        'big top right corner':
+            [[1, 1, 1,
+              1, 0, 0,
+              1, 0, 0], [3, 3]],
+        'J pointed right':
+            [[1, 0, 0,
+              1, 1, 1,], [3, 2]],
+        '9 by 9 square':
+            [[1, 1, 1,
+              1, 1, 1,
+              1, 1, 1], [9, 9]],
         '4 long line': 1
-        'bottem left big corner': 1
-        'vertical Z': 1
-        'upside down T': 1
-        'L pointed right': 1
-        '5 tall line': 1
-        'J': 1
-        'Z': 1
-        'S': 1
-        'T pointed left': 1
+            [[1, 1, 1, 1], [4, 1]],
+        'bottem left big corner':
+            [[1, 0, 0,
+              1, 0, 0,
+              1, 0, 0], [3, 3]],
+        'vertical Z':
+            [[0, 1,
+              1, 1,
+              1, 0], [2, 3]],
+        'upside down T':
+            [[0, 1, 0,
+              0, 1, 0,
+              1, 1, 1], [3, 3]],
+        'L pointed right':
+            [[1, 1, 1,
+              1, 0, 0,], [3, 2]],
+        '5 tall line':
+            [[1,
+             1,
+             1,
+             1,
+             1], [1, 5]],
+        'J':
+            [[0, 1,
+              0, 1,
+              1, 1], [2, 3]],
+        'Z':
+            [[1, 1, 0,
+              0, 1, 1], [3, 2]],
+        'S':
+            [[0, 1, 1,
+              1, 1, 0], [3, 2]],
+        'T pointed left':
+            [[0, 0, 1,
+              1, 1, 1,
+              0, 0, 1], [3, 3]]
     }
-    peaces_2 = {
-        'no block in slot 2': 1
-        '4 tall line': 1
-        '2 long line': 1
-        'top left corner': 1
-        'L pointed left': 1
-        'vertical S': 1
-        'corner top right': 1
-        '3 long line': 1
-        '2 tall line': 1
-        'T pointed right': 1
-        'S': 1
-        'small square': 1
-        '2 by 3 vertical': 1
-        'big top right corner': 1
-        'J': 1
-        'J pointed left': 1
-        'J pointed right': 1
-        '9 by 9 square': 1
-        '5 long line': 1
-        '4 long line': 1
-        '9 by 9 square': 1
-        'big bottem left corner': 1
-        'T': 1
-        'upside down J': 1
-        'upside down T': 1
-        '4 long line': 1
-        '9 by 9 square': 1
-        'big bottem right corrner': 1
-        'vertical S': 1
-        '5 long line': 1
-        'T pointed right': 1
-        '5 tall line': 1
-    }
-    peaces_3 = {
-        'no block in slot 3': 1
-        '4 long line': 1
-        '3 by 2 horizontal': 1
-        'top left corner': 1
-        'J pointed right': 1
-        '3 long line': 1
-        '2 by 3 vertical': 1
-        '4 tall line': 1
-        '2 tall line': 1
-        'S': 1
-        'Z': 1
-        '9 by 9 square': 1
-        '4 long line': 1
-        'bottem left big corner': 1
-        'vertical Z': 1
-        'upside down T': 1
-        'L pointed right': 1
-        '5 tall line': 1
-        'J': 1
-        'Z': 1
-        'S': 1
-        'T pointed left': 1
-    }
-
